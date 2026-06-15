2d41b69e5a7296e95126f6e0b255dda930396bc5
RoyABO <royespiritubravo@gmail.com>
2026-06-15 11:52:36 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "abo-cache-v1";
+
+const archivos = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icono.png"
+];
+
+self.addEventListener("install", (evento) => {
+  evento.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(archivos))
+  );
+});
+
+self.addEventListener("fetch", (evento) => {
+  evento.respondWith(
+    caches.match(evento.request)
+      .then((respuesta) => respuesta || fetch(evento.request))
+  );
+});
